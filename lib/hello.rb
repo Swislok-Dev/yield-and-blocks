77f8444c6c7d80325984b0e4c0f325f95cb150aa
cmarker <cmarker4@gmail.com>
2020-10-27 08:39:30 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,10 @@
-def hello_t
-
+def hello_t(array)
+    if block_given?
+        array.each { |name| yield(name) }
+    else
+        puts "Hey! No block was given!"
+    end
+    
 end
 
 # call your method here!
